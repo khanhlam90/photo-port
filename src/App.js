88868a24f355934313011222aa183ae85ea0d41d
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-02-14 20:00:32 -0800

    about section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // This is note
+    // React.createElement("div", {}, [
+    // React.createElement("h1", {}, "Ginger"),
+    // React.createElement("p", {}, "bread: Brittany Spaniel")
+    // ])
+    // the above is same as
+    // <div>
+      // <h1>Ginger</h1>
+      // <p>breed: Brittany Spaniel</p>
+      // </div>
+    <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
