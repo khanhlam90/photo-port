f9a229dd6fdb4b645a7ac405a3013b79b2c59f6b
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-03-02 13:58:08 -0800

    initialize the State for contact me form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    
+    //to manage the form data
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
     
     //JSX 
     return (
@@ -9,15 +14,15 @@ function ContactForm() {
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
