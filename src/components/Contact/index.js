a329d33819c5c03543862fd577eb06fdda5cfdf7
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-03-02 14:35:00 -0800

    syncing the data from the form to the state of the conponent

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,6 +7,10 @@ function ContactForm() {
 
     const { name, email, message } = formState;
     
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+    console.log(formState);
     //JSX 
     return (
         <section>
@@ -14,15 +18,15 @@ function ContactForm() {
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
